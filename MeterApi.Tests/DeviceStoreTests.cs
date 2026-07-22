1ab17e291906fe83f38d4d6b2b76c8815dcc3f93
Ahmet Erpin <github-ahmeterpin@gmail.com>
2026-07-22 15:00:26 +0300

    test: deviceStore icin birim testleri eklendi ve sablon dosyaları temizlendi

---
+++ MeterApi.Tests/DeviceStoreTests.cs
@@ -0,0 +1,73 @@
+﻿using MeterApi.Services;
+
+namespace MeterApi.Tests;
+
+public class DeviceStoreTests
+{
+    [Fact]
+    public void Add_CihaziDepoyaEkler()
+    {
+        var store = new DeviceStore();
+
+        var device = store.Add("Sayac-1", "Bodrum");
+
+        Assert.Single(store.GetAll());
+        Assert.Equal("Sayac-1", device.Name);
+        Assert.Equal("Bodrum", device.Location);
+    }
+
+    [Fact]
+    public void GetById_YoksaNullDoner()
+    {
+        var store = new DeviceStore();
+
+        var result = store.GetById(Guid.NewGuid());
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Delete_VarOlanCihaziSiler()
+    {
+        var store = new DeviceStore();
+        var device = store.Add("Sayac-2", "Cati");
+
+        var silindi = store.Delete(device.Id);
+
+        Assert.True(silindi);
+        Assert.Empty(store.GetAll());
+    }
+
+    [Fact]
+    public void Delete_OlmayanCihazIcinFalseDoner()
+    {
+        var store = new DeviceStore();
+
+        var silindi = store.Delete(Guid.NewGuid());
+
+        Assert.False(silindi);
+    }
+
+    [Fact]
+    public void AddReading_CihazaOkumaEkler()
+    {
+        var store = new DeviceStore();
+        var device = store.Add("Sayac-3", "Giris");
+
+        var reading = store.AddReading(device.Id, 42, null);
+
+        Assert.NotNull(reading);
+        Assert.Equal(42, reading!.Value);
+        Assert.Single(device.Readings);
+    }
+
+    [Fact]
+    public void AddReading_OlmayanCihazIcinNullDoner()
+    {
+        var store = new DeviceStore();
+
+        var reading = store.AddReading(Guid.NewGuid(), 10, null);
+
+        Assert.Null(reading);
+    }
+}
